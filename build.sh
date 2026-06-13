f0fb34300c8743a2844b2532afa7a13bc7ac0d93
DavidMRB <davidrangelbaez@gmail.com>
2026-06-12 22:10:26 -0500

    Add all required opus libraries

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 apt-get update
-apt-get install -y ffmpeg libopus-dev libffi-dev libssl-dev python3-dev
-pip install --upgrade pip
+apt-get install -y \
+  ffmpeg \
+  libopus-dev \
+  libopus0 \
+  libffi-dev \
+  libssl-dev \
+  python3-dev \
+  build-essential \
+  git
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
