5689e0192e45840adb7e2b64a915c214e0a2e034
DavidMRB <davidrangelbaez@gmail.com>
2026-06-12 22:07:38 -0500

    Add more dependencies for voice support

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get install -y ffmpeg libopus-dev
+apt-get install -y ffmpeg libopus-dev libffi-dev libssl-dev python3-dev
+pip install --upgrade pip
 pip install -r requirements.txt
